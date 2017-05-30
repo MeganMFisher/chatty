4fe53293bd6a38868f57ec7edfe916e8ed1d2183
MeganMFisher <meganmichelle318@gmail.com>
2017-05-30 11:30:31 -0600

    updates

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var messages = [];
 var usernames = [];
+
 app.use(express.static('assets'))
 app.use(bodyParser.json())
 
@@ -20,3 +21,26 @@ app.post('/messages', function(req, res, next){
 app.listen(3000, function(){
     console.log('listening on port 3000');
 })
+
+// const express = require('express')
+// const bodyParser = require('body-parser')
+
+// const app = express()
+
+// app.use(express.static('assets'))
+// app.use(bodyParser.json())
+
+// let messages = [];
+
+
+// app.get('/messages', function(req, res, next) {
+//   res.status(200).json({ messages: messages });
+// });
+
+
+// app.post('/messages', function(req, res, next) {
+//   messages.push({ message: req.body.message, time: new Date() });
+//   res.status(200).json({ messages: messages });
+// });
+
+// app.listen(3000)
